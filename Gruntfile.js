6bd0a79dbf0cfab86440a4d27708441babd6abfc
Chris Droom <chris.droom@gmail.com>
2015-07-03 20:42:14 +0800

    add grunt watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,15 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
+
+    watch: {
+      sass: {
+        files: ['css/*.sass'],
+        tasks: ['sass']
+      }
+    },
+
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
@@ -24,20 +33,25 @@ module.exports = function(grunt) {
       }
     }
 
+
+
   });
 
 
 
+  // watch me now
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
 
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  // Sass it ups
+  // Sass it up
   grunt.loadNpmTasks('grunt-contrib-sass');
 
+
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['uglify', 'sass']);
 
 };
