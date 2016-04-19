3bfa375a54cff1acc8e6ab1b2469f8492bbc9ed5
droom <chrisdroom@gmail.com>
2016-04-19 02:45:04 +0100

    formalise flex

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,34 @@
 $( document ).ready(function() {
 
-  var returnShow = new Waypoint({
-    element: document.getElementById('steps'),
-    handler: function(direction) {
-      $("#steps ol").addClass('turn');
-    }, offset: 200
-  })
+	var returnShow = new Waypoint({
+		element: document.getElementById('steps'),
+		handler: function(direction) {
+			$("#steps ol").addClass('turn');
+		}, offset: 200
+	})
+
+
+
+	var vidPaused = true
+
+	$('#play').click(function() {
+		
+		if (vidPaused) {
+
+			$('#movie')[0].play();
+			vidPaused = false;
+
+		} else {
+
+			$('#movie')[0].pause(); 
+			vidPaused = true;
+		}
+
+		console.log("hey");
+		return false;
+		// vidPaused =! vidPaused;
+
+	});
 
 
 
