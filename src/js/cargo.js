d0b188141833ad641c4ebf94ca96831d4b871fee
droom <chrisdroom@gmail.com>
2016-05-30 19:01:06 +0100

    cargo

---
+++ src/js/cargo.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+	// $('#cargo-steps').css('order', '4');
+	// $('#cargo-video').css('order', '3');
+	// $('#cargo-quad'	).css('order', '6');
+	// $('#cargo-app'	).css('order', '1');
+	// $('#cargo-quote').css('order', '2');
+	// $('#cargo-earn'	).css('order', '5');
+});
